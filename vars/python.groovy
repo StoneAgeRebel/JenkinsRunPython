acd9c543d1bf7bd3ed9d1470a94a5129942c1a63
Henry Borchers <henryborchers@yahoo.com>
2017-08-09 23:03:25 -0500

    size

--- vars/python.groovy
+++ vars/python.groovy
@@ -2,4 +2,6 @@ def call(String python_script){
     echo "running Python on " + python_script
     def pyFile = file(python_script)
     println("I got a file and it's existance is $pyFile")
+    def s = pyFile.size()
+    println("I got a file and it's existance is $s")
 }
