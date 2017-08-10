574a03adc8cf96807cc2081fda3fab8f1487d990
Henry Borchers <henryborchers@yahoo.com>
2017-08-09 23:11:48 -0500

    exists

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,7 +1,12 @@
 def call(String python_script){
     echo "running Python on " + python_script
     def pyFile = new File(python_script)
-    println("I got a file and it's existance is $pyFile")
-    def s = pyFile.size()
-    println("I got a file and it's size is $s")
+    if(fileExists(python_script)){
+        echo "it exists"
+    } else {
+        echo "no exist"
+    }
+//    println("I got a file and it's existance is $pyFile")
+//    def s = pyFile.size()
+//    println("I got a file and it's size is $s")
 }
