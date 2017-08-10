6f901ee7ac3c2c769472548e8fb887f50c5f274e
Henry Borchers <henryborchers@yahoo.com>
2017-08-09 22:21:15 -0500

    quotes

--- vars/python.groovy
+++ vars/python.groovy
@@ -3,6 +3,6 @@ def call(String python_script = "dummy"){
     echo python_script
     def files = findFiles(glob: "*.*")
 //    def files = findFiles(python_script)
-//    echo files
+    echo "files = ${files}"
 
 }
