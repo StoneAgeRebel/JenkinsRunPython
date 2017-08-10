22f496e73485875f0306639e7f39a4987ee84b65
Henry Borchers <henryborchers@yahoo.com>
2017-08-09 22:44:43 -0500

    removed useless code

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,16 +1,3 @@
 def call(String python_script = "dummy"){
-    echo "running Python"
-    echo python_script
-    def files = findFiles(glob: "**/*.*")
-//    def files = findFiles(python_script)
-//    println(files.type)
-    echo "files = ${files}"
-    println(files)
-//    println(files.getPath())
-    println(files[0])
-    echo "getPath ${files[0].path}"
-    println(files[0].getPath())
-    echo "getName ${files[0].name}"
-    println(files[0].getName())
-
+    echo "running Python on " + python_script
 }
