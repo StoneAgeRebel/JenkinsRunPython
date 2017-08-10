69ab9704c3b9e331cba644fade5bfc9a750272b1
Henry Borchers <henryborchers@yahoo.com>
2017-08-09 22:23:19 -0500

    println

--- vars/python.groovy
+++ vars/python.groovy
@@ -3,6 +3,8 @@ def call(String python_script = "dummy"){
     echo python_script
     def files = findFiles(glob: "*.*")
 //    def files = findFiles(python_script)
+//    println(files.type)
+    println(files)
     echo "files = ${files}"
 
 }
