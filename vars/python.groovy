1173024f4861d75791c2bf7f95d6b9eb53dbfe9e
Henry Borchers <henryborchers@yahoo.com>
2017-08-09 22:18:56 -0500

    glob

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,7 +1,7 @@
 def call(String python_script = "dummy"){
     echo "running Python"
     echo python_script
-    def files = findFiles("*.*")
+    def files = findFiles(glob: "*.*")
 //    def files = findFiles(python_script)
     echo files
 
