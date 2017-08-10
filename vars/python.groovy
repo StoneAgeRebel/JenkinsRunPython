9709e5403f6430ee1720f2314a123641924024d7
Henry Borchers <henryborchers@yahoo.com>
2017-08-09 23:01:14 -0500

    reverted

--- vars/python.groovy
+++ vars/python.groovy
@@ -2,5 +2,4 @@ def call(String python_script){
     echo "running Python on " + python_script
     def pyFile = file(python_script)
     println("I got a file and it's existance is $pyFile")
-    println("pyFile.path = $pyFile.path")
 }
